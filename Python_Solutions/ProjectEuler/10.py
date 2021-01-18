bb79154ee9ac9f756fbf26b4e8f060d694c819ba
jakobau <30506552+jakobau@users.noreply.github.com>
2021-01-17 17:01:39 -1000

    Update 10.py

--- Python_Solutions/ProjectEuler/10.py
+++ Python_Solutions/ProjectEuler/10.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # problem 10
 # Summation of primes
 #
@@ -5,19 +6,34 @@
 
 import math
 
-number = 200000#2000000 # 2 million
+number = 200000 # 2 million
 
-count = 1
-num = 1
+answer = 3
+num = 3
+primeNums = [1,2]
+count = 2
 
 while num < number :
     isPrime = True
-    for i in range (2,num-1) :
-        if num%i == 0 : # not a prime
+    print(str(num))
+
+    #for i in range (3,math.ceil((num-1)/2),2) :
+    #    if num%i == 0 : # not a prime
+    #        isPrime = False
+    #        break
+    #for i in range (2,math.ceil(len(primeNums)/2)) :
+
+    for i in range (2,math.ceil(len(primeNums)/2)) :
+        if num%primeNums[i] == 0 :
             isPrime = False
             break
+
     if isPrime :
-        count+=num
+        primeNums.append(num)
+        answer+=num
+        count+=1
     num+=2
 
-print(count)
+print("Number of Primes: " + str(count))
+print("Answer: " + str(answer))
+#print("Prime Numbers: " + str(primeNums))
